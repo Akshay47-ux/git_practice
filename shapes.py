91296018864ea65993d8101828616c1c84fff14b
Akshay47-ux <akshayega@gmail.com>
2025-07-25 19:15:40 +0530

    removed unwated spces

--- shapes.py
+++ shapes.py
@@ -2,13 +2,11 @@ class Shape:
     def area(self):
         print("The area method is not implemented")
 
-
 class Circle(Shape):
     def __init__(self, radius):
         self.radius=radius
     def area(self):
         return 3.14 * self.radius * self.radius
-
 
 class Rect(Shape):
     def __init__(self, l, b):
@@ -17,16 +15,13 @@ class Rect(Shape):
     def area(self):
         return self.l * self.b
 
-
 def main():
     radius = int(input("Enter the radius: "))
     c = Circle(radius)
     print("Area of Circle:", c.area())
-
+    
     l = int(input("Enter length: "))
     b = int(input("Enter breadth: "))
     r = Rect(l, b)
     print("Area of Rectangle:", r.area())
-
-
 main()
