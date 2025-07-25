5daebf9a5d07164cb048d4d41d981db20a7b2fb4
Akshay47-ux <akshayega@gmail.com>
2025-07-25 15:19:12 +0530

    added vehicle.py

---
+++ vehicle.py
@@ -0,0 +1,17 @@
+class vehicle:
+    def __init__(self):  
+        pass    
+    def start(self):
+        print("the vehicle has started")
+    def stop(self):
+        print("engine has stopped")
+#v=vehicle()       
+class car(vehicle):
+    def music(self):
+        print("music playing")
+def main():
+    c=car()
+    c.start()
+    c.music()
+    c.stop()
+main()
